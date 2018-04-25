9423c6571879acae370c70c6de405b8066b35386
Bryan Yeap <byeap@ggc.edu>
2018-04-25 17:22:28 -0400

    Fixed Typo

--- src/main/java/com/example/vehicleproject2/CustomAspect.java
+++ src/main/java/com/example/vehicleproject2/CustomAspect.java
@@ -17,19 +17,19 @@ public class CustomAspect {
     public void custom(final JoinPoint joinPoint) {
         String point = joinPoint.getSignature().toString();
         if(point.indexOf("addVehicle(") >= 0) {
-            System.out.println("Custon: POST request called");
+            System.out.println("Custom: POST request called");
         }
         else if(point.indexOf("getVehicle(") >= 0) {
-            System.out.println("Custon: GET request called");
+            System.out.println("Custom: GET request called");
         }
         else if(point.indexOf("removeVehicle(") >= 0) {
             System.out.println("Custon: DELETE request called");
         }
         else if(point.indexOf("updateVehicle(") >= 0) {
-            System.out.println("Custon: PUT request called");
+            System.out.println("Custom: PUT request called");
         }
         else if(point.indexOf("getLatestVehicles(") >= 0){
-            System.out.println("Custon: GET request called");
+            System.out.println("Custom: GET request called");
         }
     }
 }
